d8acdcc96d78d400046fbaf78b1edd7c5af6f9b8
SchreinerK <ks@ksware.de>
2018-02-14 22:02:58 +0100

    .

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -14,30 +14,15 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("f6a04605-0932-4516-9f0e-ab95c7eae9c3")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0")]
 
 namespace KsWare {
 
 	static partial class Console {
+
 		public static class AssemblyInfo { }
 	}
 }
